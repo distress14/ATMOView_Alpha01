d3191f1ea22e3dbf295524a298568b74e24cb59e
Andrea <110169272+distress14@users.noreply.github.com>
2024-03-20 13:07:22 +0100

    Added InputSanification-ValidSensorType
    Check of OutOfBonds and selection of valid Sensor

--- include/Sensor.h
+++ include/Sensor.h
@@ -1,4 +1,30 @@
 #ifndef SENSOR_H
 #define SENSOR_H
 
+class Sensor
+{
+private:
+    const float posX, posY;    //Using const cause the position will be fixed once selected, and float (32bit) instead of double (64 bit)
+    const int Identification;   //ID-Identification will not change once selected.
+    const int SensorType;   //Needs to be 0-4 (max 5 Sensors). It will be the ID for the Sensor type choosed.  //TODO SANIFICATION of INPUT
+    int LastUpdate;     // Keep tracking time of last update of the XML file (if used)
+    float SensorValue;         //Actual value of the Sensor (Not used int cause I wanna the PRECISION)
+    float SensorValueMax;    //If needed, a bottom value (es:temperature)
+    float SensorValueMin;   //If needed, a maximum value (es:temperature)
+    float LastSensorValue;    //It can be used to do the comparation PRE-ACTUAL in (+/-)%
+    const bool FromXML;    //Bool const which tells me if the User choosed to use an XML file. 0: Not Used | 1: Used -> Do not start Simulation for that Sensor
+
+public:
+    Sensor();  //constructor of the Class Sensor
+    ~Sensor();  //Deconstructor of the Class Sensor
+    bool InputSanification();    //Checks if the User was that gentle to use a valid Input during the "Form". Returns 1 if OK //TODO InputForm
+    bool IsOutOfBonds(const float posX, const float posY);    //Checks if the User used a valid position within the Map bounds. Returns 0 if OK
+    bool ValidSensorType(const int SensorType);   //Checks if the User choosed an avaible Sensor. Returns 1 if OK
+
+    //TODO
+    //void CalculateSensorValue(float SensorValue);   //It does every calculation on the actual value and returns it updated.
+};
+
+
+
 #endif // SENSOR_H
--- src/Sensor.cpp
+++ src/Sensor.cpp
@@ -1,35 +1,75 @@
 #include <iostream>
 #include <math.h>
+#include "../include/Sensor.h"
 
+const int MAX_X = 50;
+const int MAX_Y = 50;
+const int MIN_X = 0;
+const int MIN_Y = 0;
 
-class Sensor
+Sensor::Sensor()
 {
-private:
-    const float posX, posY;    //Using const cause the position will be fixed once selected, and float (32bit) instead of double (64 bit)
-    const int Identification;   //ID-Identification will not change once selected.
-    const int SensorType;   //Needs to be 0-4 (max 5 Sensors). It will be the ID for the Sensor type choosed.  //TODO SANIFICATION of INPUT
-    int LastUpdate;     // Keep tracking time of last update of the XML file (if used)
-    float SensorValue;         //Actual value of the Sensor (Not used int cause I wanna the PRECISION)
-    float SensorValueMax;    //If needed, a bottom value (es:temperature)
-    float SensorValueMin;   //If needed, a maximum value (es:temperature)
-    float LastSensorValue;    //It can be used to do the comparation PRE-ACTUAL in (+/-)%
-    const bool FromXML;    //Bool const which tells me if the User choosed to use an XML file. 0: Not Used | 1: Used -> Do not start Simulation for that Sensor
+    //TODO
+}
 
-public:
-/*
- * void NewSensor()   //Adds 1 new Sensor to the SXSideSensor tab. Need to POP-UP an "alert" (New window) in which must be added ID - SensorType - FromXML. Remaining are optional
+ //TODO == Adds 1 new Sensor to the SXSideSensor tab. Need to POP-UP an "alert" (New window) in which must be added ID - SensorType - FromXML. Remaining are optional
+
+
+bool Sensor::InputSanification()
+{
+    if (IsOutOfBonds(posX, posY) || (! ValidSensorType(SensorType)))    //Checks if IsOutOfBonds && if ValidSensorType IS NOT true
     {
-        if (InputSanification() == 0){    //Calls the [TODO bool InputSanification function], which returns True if the Input is respected, 0 otherwise
-            std::cout << "Something went wrong with the Input. Error: 001 " << std::endl;   //Error 001 - Errors will be "sequential", so I can track easly where the error is
-            return 0;      //Exit the program. TODO --> Display Error on the "alert" window and recall a NEW "alert" window, asking gently if you can choose between 0-5
-        }
-
-
-    }
-    void DeleteSensor()
-    {
-
+        std::cout << "Something went wrong with the Input. Error: 001 " << std::endl;   //Error 001 - Errors will be "sequential", so I can track easly where the error is
+        return 0;      //Exit the program. TODO --> Display Error on the "alert" window and recall a NEW "alert" window, asking gently if you can choose between 0-5
     }
 
-*/
-};
+    return 1;        //Input is OK
+}
+
+
+bool Sensor::IsOutOfBonds(const float User_X, const float User_Y)
+{
+    if ((User_X > MAX_X) || (User_Y > MAX_Y))      //Checks if the User choosed a valid position within the max_BoundlLmit of the map
+    {
+        std::cout << "Cazzone Hai sbagliato a mettere una posizione decente. Mettine una valida" << std::endl;
+        std::cout << "Error 002" << std::endl;
+        return 0;
+    }
+    if ((User_X < MIN_X) || (User_Y < MIN_Y))       //Checks if the User choosed a valid position within the min_BoundlLmit of the map
+    {
+        std::cout << "Cazzone Hai sbagliato a mettere una posizione decente. Mettine una valida" << std::endl;
+        std::cout << "Error 003" << std::endl;
+        return 0;
+    }
+
+    return 1;     //Bound is OK
+}
+
+
+bool Sensor::ValidSensorType()       //Checks if the User choosed an avaible Sensor. Returns 1 if OK. 0 otherwise
+{                                       //TODO code inside Cases
+    switch (SensorType)
+    {
+    case 0:
+        std::cout << "You choose the Sensor #0 " << std::endl;
+        break;
+    case 1:
+        std::cout << "You choose the Sensor #1 " << std::endl;
+        break;
+    case 2:
+        std::cout << "You choose the Sensor #2 " << std::endl;
+        break;
+    case 3:
+        std::cout << "You choose the Sensor #3 " << std::endl;
+        break;
+    case 4:
+        std::cout << "You choose the Sensor #4 " << std::endl;
+        break;
+    default:
+        std::cout << "Bastardo scegli bene" << std::endl;
+        return 0;
+
+    }
+    return 1;  //Type is OK
+}
+
